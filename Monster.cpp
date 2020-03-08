ddf9212edc8b64dcb8c72061ac413ce483705174
DournauxNathan <dournauxnathan@gmail.com>
2020-03-08 12:42:40 +0100

    Update

--- Monster.cpp
+++ Monster.cpp
@@ -2,12 +2,24 @@
 using namespace std;
 
 myTeam::myTeam() : m_vie(50), m_attaque1(5), m_attaque2(10), m_attaque3(5), m_attaque4(5){}
+myTeam::Monster() : m_vie(50), m_attaque1(5), m_attaque2(10), m_attaque3(5), m_attaque4(5){}
 
 int myTeam::getVie(){
   return m_vie;
 }
 
 int myTeam::getAttaque() {
+  return m_attaque1;
+  return m_attaque2;
+  return m_attaque3;
+  return m_attaque4;
+}
+
+int Monster::getVie(){
+  return m_vie;
+}
+
+int Monster::getAttaque() {
   return m_attaque1;
   return m_attaque2;
   return m_attaque3;
