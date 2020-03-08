959f9e02dcfc39f1c0cfe04260449fd96ee7d7fe
DournauxNathan <dournauxnathan@gmail.com>
2020-03-08 12:39:02 +0100

    Caractéristiques
    - Déclaration de  l'ensemble des caractéristique au monstre alliés dans Monster.h.
    - Initialisation de ces caractéristque dans Monster.cpp.

--- Monster.h
+++ Monster.h
@@ -1,1 +1,16 @@
+using namespace std;
 
+class myTeam {
+    private:
+    int m_vie;
+    int m_attaque1;
+    int m_attaque2;
+    int m_attaque3;
+    int m_attaque4;
+    
+    public: 
+    int getVie();
+    int getAttaque();
+    void setPv(int);
+    myTeam();
+};
