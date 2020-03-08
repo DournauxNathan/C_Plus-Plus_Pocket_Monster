3fa1e8aae0ac33be276dfd336a6e6c96cac9abc2
DournauxNathan <dournauxnathan@gmail.com>
2020-03-08 14:01:04 +0100

    Combat
    Intégration du système de combat.
    -Le joeur peut choisir 1/4 attaque.
    (Réduction de points vie de l'ennemi)
    -L'ennemi choiside façon aleatoire l'attaque que le joueur recevra.

--- Monster.cpp
+++ Monster.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 myTeam::myTeam() : m_vie(50), m_attaque1(5), m_attaque2(10), m_attaque3(5), m_attaque4(5){}
-myTeam::Monster() : m_vie(50), m_attaque1(5), m_attaque2(10), m_attaque3(5), m_attaque4(5){}
+myTeam::Monster() : m_vie(50), m_attaque1(5), m_attaque2(10), m_attaque3(5), m_attaque4(10){}
 
 int myTeam::getVie(){
   return m_vie;
@@ -15,6 +15,12 @@ int myTeam::getAttaque() {
   return m_attaque4;
 }
 
+void myTeam::setVie(int _pv){
+
+  pv += _pv;
+  
+};
+
 int Monster::getVie(){
   return m_vie;
 }
@@ -25,3 +31,9 @@ int Monster::getAttaque() {
   return m_attaque3;
   return m_attaque4;
 }
+
+void Monster::setVie(int _pv){
+
+  pv += _pv;
+  
+};
